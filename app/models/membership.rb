810eb84fc40b3585bd7685b9a7e5b4fd38198374
Jakob Skjerning <jakob@mentalized.net>
2019-11-03 15:37:40 +0100

    Fix formatting

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-# A Membership is the participant in a group. It is different from a User in that a
-# Membership doesn't have to bear any resemblance to a real person.
+# A Membership is a users participation in a group. It is different from a User
+# in that a Membership doesn't have to bear any resemblance to a real person.
 #
-# A Membership is the way a User decides to present themselves to a specific Group.
+# Basically, this is how the user chooses to represent herself in a specific
+# group and her preferences for how to interact with that group.
 #
 # Basically, Membership is controlled by the User, whereas Member is controlled
 # by the Group (owners).
