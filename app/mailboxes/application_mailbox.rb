4fa549ddab0c7e061b60669378ba4642d0c929cb
Jakob Skjerning <jakob@mentalized.net>
2020-02-07 23:02:33 +0100

    Add a posts mailbox

--- app/mailboxes/application_mailbox.rb
+++ app/mailboxes/application_mailbox.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationMailbox < ActionMailbox::Base
-  # routing /something/i => :somewhere
+  routing(/^@posts\./i => :posts)
 end
---
+++ app/mailboxes/posts_mailbox.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class PostsMailbox < ApplicationMailbox
+  def process; end
+end
