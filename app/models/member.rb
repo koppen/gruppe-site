d6cd6cca554462bfc1872c48987c6d16b1cabba4
Jakob Skjerning <jakob@mentalized.net>
2020-02-08 21:34:44 +0100

    Document a bit more

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 # A connection between a Group and a User
+#
+# This is the group-controllable part of a Users participation in a Group.
 class Member < ApplicationRecord
   belongs_to :group
   belongs_to :membership, :dependent => :destroy
