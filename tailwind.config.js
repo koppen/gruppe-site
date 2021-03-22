fd59a47adcb251a4555a5ec06b472643682b920d
Jakob Skjerning <jakob@mentalized.net>
2021-03-22 09:24:23 +0100

    Update members to Tailwind 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rose: colors.rose,
+      }
+    }
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
